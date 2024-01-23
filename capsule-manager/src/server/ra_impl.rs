bcde4ac0475b40eb11f08231e6d7a87115321926
He Sun <sunhe2007@gmail.com>
2024-01-23 08:43:32 +0000

    Use coco attester

--- capsule-manager/src/server/ra_impl.rs
+++ capsule-manager/src/server/ra_impl.rs
@@ -16,7 +16,6 @@ use super::CapsuleManagerImpl;
 use crate::server::constant::SEPARATOR;
 use ::capsule_manager::errno;
 use ::capsule_manager::error::errors::{AuthResult, Error, ErrorCode, ErrorLocation};
-use ::capsule_manager::remote_attestation::unified_attestation_wrapper::runified_attestation_generate_auth_report;
 use ::capsule_manager::utils::tool::sha256;
 use capsule_manager_tonic::secretflowapis::v2::sdc::capsule_manager::{
     GetRaCertRequest, GetRaCertResponse,
@@ -25,6 +24,8 @@ use capsule_manager_tonic::secretflowapis::v2::sdc::{
     UnifiedAttestationReport, UnifiedAttestationReportParams,
 };
 use capsule_manager_tonic::secretflowapis::v2::{Code, Status};
+
+use attester::BoxedAttester;
 use hex::encode_upper;
 use log::debug;
 
@@ -33,53 +34,34 @@ impl CapsuleManagerImpl {
         &self,
         request: &GetRaCertRequest,
     ) -> AuthResult<GetRaCertResponse> {
-        // the fixed data
-        let tee_identity: &str = "1";
-        let hex_report_type = "Passport".to_string();
-        // generate one report for nonce
-
         let attestation_report: Option<UnifiedAttestationReport> = match self.mode.as_str() {
             // get RA report
             "production" => {
                 let data = [&self.kek_cert, request.nonce.as_bytes()].join(SEPARATOR.as_bytes());
-                // fill report params
-                let report_params = UnifiedAttestationReportParams {
-                    str_report_identity: "".to_owned(),
-                    hex_user_data: encode_upper(sha256(&data)),
-                    json_nested_reports: "".to_owned(),
-                    hex_spid: "".to_owned(),
+                let hex_user_data = encode_upper(sha256(&data));
+                let boxed_attester =
+                    BoxedAttester::try_from(attester::detect_tee_type()).map_err(|e| {
+                        errno!(ErrorCode::UnsupportedErr, "unsupported TEE type: {:?}", e)
+                    })?;
+                // get base64-encoded quote
+                let evidence = boxed_attester
+                    .get_evidence(hex_user_data.as_bytes().to_vec())
+                    .await
+                    .map_err(|e| {
+                        errno!(
+                            ErrorCode::InternalErr,
+                            "failed to get sgx quote err: {:?}",
+                            e
+                        )
+                    })?;
+                let report = UnifiedAttestationReport {
+                    str_report_version: "1.0".to_string(),
+                    str_report_type: "JD".to_string(),
+                    str_tee_platform: "SGX_DCAP".to_string(),
+                    json_report: evidence,
+                    json_nested_reports: String::new(),
                 };
-
-                let report_json = runified_attestation_generate_auth_report(
-                    tee_identity,
-                    hex_report_type.as_str(),
-                    "",
-                    serde_json::to_string(&report_params)
-                        .map_err(|e| {
-                            errno!(
-                                ErrorCode::InternalErr,
-                                "report_params {:?} to json err: {:?}",
-                                &report_params,
-                                e
-                            )
-                        })?
-                        .as_str(),
-                )
-                .map_err(|e| {
-                    errno!(
-                        ErrorCode::InternalErr,
-                        "runified_attestation_generate_auth_report err: {:?}",
-                        e
-                    )
-                })?;
-                Some(serde_json::from_str(report_json.as_str()).map_err(|e| {
-                    errno!(
-                        ErrorCode::InternalErr,
-                        "json {:?} to attentation_report err: {:?}",
-                        &report_json,
-                        e
-                    )
-                })?)
+                Some(report)
             }
             // simulation mode doesn't need report
             "simulation" => None,
